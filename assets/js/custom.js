beaa85731c713029b5e089c3447f31401d3ad246
deep thummar <deepthummmar@gmail.com>
2024-02-15 17:58:11 +0530

    creating slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,3 +10,17 @@ jQuery('.go_to_top').on('click', function(e) {
         scrollTop: 0
     }, 300);
 });
+
+$('.center').slick({
+    centerMode: true,
+    infinite: true,
+    centerPadding: '0px',
+    speed: 500,
+    variableWidth: true,
+  });
+  $('.center').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    console.log('beforeChange', currentSlide, nextSlide);
+  });
+  $('.center').on('afterChange', function(event, slick, currentSlide){
+    console.log('afterChange', currentSlide);
+  });
